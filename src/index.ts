941705138609e4d23738447d1d5239dc450926f0
Kıraç Armağan Önal <kiracarmaganonal@gmail.com>
2022-09-05 17:34:15 +0300

    published to npm

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { DBI, DBIConfig } from "./DBI";
 export { recursiveImport } from "./utils/recursiveImport";
+export { MemoryStore } from "./utils/MemoryStore";
 
 export function createDBI(namespace: string, cfg: DBIConfig): DBI {
   return new DBI(namespace, cfg);
