8edef46545bec931b657f77c55ade116acf2f1e8
Erdem <74366943+ErdemGKSL@users.noreply.github.com>
2022-09-30 16:29:23 +0300

    Generated Path Export

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import { NamespaceEnums } from "../generated/namespaceData";
 import { DBI, DBIConfigConstructor } from "./DBI";
+import path from "path";
 export { recursiveImport } from "./utils/recursiveImport";
 export { MemoryStore } from "./utils/MemoryStore";
-
+export const generatedPath = path.resolve(__dirname, "../generated");
 export function createDBI<TNamespace extends NamespaceEnums, TOtherType = Record<string, any>>(namespace: TNamespace, cfg: DBIConfigConstructor): DBI<TOtherType, TNamespace> {
   return new DBI<TOtherType, TNamespace>(namespace, cfg);
 }
