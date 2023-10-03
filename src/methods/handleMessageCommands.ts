fcbf1a5d2ecf1784d80af748a2189bdb3b0dd37a
Erdem <zahrakon06@gmail.com>
2023-10-03 22:49:16 +0300

    better stuff right there

--- src/methods/handleMessageCommands.ts
+++ src/methods/handleMessageCommands.ts
@@ -1,53 +1,99 @@
-import { Message, MessagePayload } from "discord.js";
+import { ApplicationCommandType, ChatInputCommandInteraction, Locale, Message, MessagePayload } from "discord.js";
 import { NamespaceEnums } from "../../generated/namespaceData";
 import { DBI } from "../DBI";
+import { FakeMessageInteraction } from "../types/other/FakeMessageInteraction";
 
-export function handleMessageCommands(dbi: DBI<NamespaceEnums>, message: Message) {
+export async function handleMessageCommands(dbi: DBI<NamespaceEnums>, message: Message) {
   const chatInputs = dbi.data.interactions.filter(i => i.type === "ChatInput");
   const prefixes = dbi.config.messageCommands.prefixes ?? [];
+  if (!prefixes.length) return;
   const content = message.content;
   const usedPrefix = prefixes.find(p => content.startsWith(p));
   if (!usedPrefix) return;
   const contentWithoutPrefix = content.slice(usedPrefix?.length ?? 0);
   const contentLower = contentWithoutPrefix.toLowerCase();
-  const chatInput = chatInputs.find(i => contentLower.startsWith(i.name));
-  if (!chatInput) return;
 
-  let repliedMessage: Message;
-  let lastFollowUp: Message;
-  let obj  = {
-    channelId: message.channel.id,
-    async deferReply(options: { ephemeral: boolean }) {
-      if (options.ephemeral) throw new Error("Ephemeral replies are not supported in message commands.");
-      if (repliedMessage) throw new Error("Already deferred reply.");
-      repliedMessage = await message.reply("Loading...");
-      return repliedMessage;
-    },
-    async deleteReply() {
-      if (!repliedMessage) throw new Error("No deferred reply.");
-      await repliedMessage.delete();
-      repliedMessage = undefined;
-    },
-    async followUp(content: string | MessagePayload) {
-      if (!repliedMessage) throw new Error("No deferred reply.");
-      if (!lastFollowUp) {
-        lastFollowUp = await repliedMessage.reply(content);
-      } else {
-        lastFollowUp = await lastFollowUp.reply(content);
+  let locale: Locale = message.guild.preferredLocale || dbi.config.defaults.locale as any;
+  let chatInput = chatInputs.find(i => contentLower.startsWith(i.name));
+  let commandName = chatInput?.name;
+
+  if (!chatInput) {
+    fLoop: for (const [localeInterName, localeData] of dbi.data.interactionLocales) {
+      for (const [localeName, translation] of Object.entries(localeData.data || {})) {
+        if (contentLower.startsWith(translation.name)) {
+          commandName = translation.name;
+          locale = localeName as any;
+          chatInput = chatInputs.find(i => i.name === localeData.name);
+          break fLoop;
+        }
       }
-      return lastFollowUp;
-    },
-    async editReply(content: string | MessagePayload) {
-      if (!repliedMessage) throw new Error("No deferred reply.");
-      await repliedMessage.edit(content);
-      return repliedMessage;
-    },
-    async reply(content: string | MessagePayload) {
-      if (repliedMessage) throw new Error("Already deferred reply.");
-      repliedMessage = await message.reply(content);
-      return repliedMessage;
     }
   }
 
-  dbi.client().client.emit("interactionCreate", obj as any);
+  if (!chatInput) return;
+
+  // let repliedMessage: Message;
+  // let lastFollowUp: Message;
+  // let obj: ChatInputCommandInteraction = {
+  //   channelId: message.channel.id,
+  //   commandName: chatInput.name,
+  //   appPermissions: message.guild.members.me.permissionsIn(message.channel as any),
+  //   applicationId: message.client.user.id,
+  //   channel: message.channel as any,
+  //   command: null,
+  //   commandGuildId: message.guild.id,
+  //   commandId: null,
+  //   commandType: ApplicationCommandType.ChatInput,
+  //   awaitModalSubmit() {
+  //     throw new Error("Method not implemented.");
+  //   },
+  //   async fetchReply() {
+  //     return  repliedMessage?.id && await message.channel.messages.fetch(repliedMessage.id);
+  //   },
+  //   deferred: false,
+  //   client: message.client,
+  //   createdAt: message.createdAt,
+  //   ephemeral: false,
+  //   createdTimestamp: message.createdTimestamp,
+  //   guild: message.guild,
+  //   guildId: message.guild.id,
+  //   guildLocale: message.guild.preferredLocale,
+  //   id: message.id,
+  //   inGuild() { return true; },
+  //   async deferReply(options: { ephemeral: boolean }) {
+  //     if (options.ephemeral) throw new Error("Ephemeral replies are not supported in message commands.");
+  //     if (repliedMessage) throw new Error("Already deferred reply.");
+  //     repliedMessage = await message.reply("Loading...");
+  //     this.deferred = true;
+  //     return repliedMessage;
+  //   },
+  //   async deleteReply() {
+  //     if (!repliedMessage) throw new Error("No deferred reply.");
+  //     await repliedMessage.delete();
+  //     repliedMessage = undefined;
+  //   },
+  //   async followUp(content: string | MessagePayload) {
+  //     if (!repliedMessage) throw new Error("No deferred reply.");
+  //     if (!lastFollowUp) {
+  //       lastFollowUp = await repliedMessage.reply(content);
+  //     } else {
+  //       lastFollowUp = await lastFollowUp.reply(content);
+  //     }
+  //     return lastFollowUp;
+  //   },
+  //   async editReply(content: string | MessagePayload) {
+  //     if (!repliedMessage) throw new Error("No deferred reply.");
+  //     await repliedMessage.edit(content);
+  //     return repliedMessage;
+  //   },
+  //   async reply(content: string | MessagePayload) {
+  //     if (repliedMessage) throw new Error("Already deferred reply.");
+  //     repliedMessage = await message.reply(content);
+  //     return repliedMessage;
+  //   }
+  // }
+
+  const interaction = new FakeMessageInteraction(message, chatInput, locale, commandName);
+
+  dbi.client().client.emit("interactionCreate", interaction as any);
 }
---
+++ src/types/other/FakeMessageInteraction.ts
@@ -0,0 +1,134 @@
+import { Message, MessagePayload, ApplicationCommandType, ChatInputCommandInteraction, Locale, APIInteractionGuildMember, GuildMember, PermissionsBitField, CacheType, CommandInteractionOptionResolver, CommandOptionDataTypeResolvable, ApplicationCommandOptionType } from 'discord.js';
+import { TDBIInteractions } from '../Interaction';
+
+export class FakeMessageInteraction /* implements ChatInputCommandInteraction */ {
+  channelId: string;
+  commandName: string;
+  appPermissions: any;
+  applicationId: string;
+  channel: any; 
+  command: any;
+  commandGuildId: string;
+  commandId: any; 
+  commandType: ApplicationCommandType.ChatInput;
+  // awaitModalSubmit: (...arr: any[]) => any;
+  // fetchReply: () => Promise<any>;
+  deferred: boolean = false;
+  client: any; 
+  createdAt: Date;
+  ephemeral: boolean = false;
+  createdTimestamp: number;
+  guild: any; 
+  guildId: string;
+  guildLocale: Locale;
+  id: string;
+  private repliedMessage: Message | undefined;
+  private lastFollowUp: Message | undefined;
+  member: GuildMember | APIInteractionGuildMember;
+  memberPermissions: Readonly<PermissionsBitField>;
+  private parsedArgs = new Map<string, FakeMessageInteractionArgument>();
+
+  constructor(private message: Message, chatInput: TDBIInteractions<string | number>, public locale: Locale, commandName: string) {
+    this.channelId = message.channel.id;
+    this.commandName = chatInput.name;
+    this.appPermissions = message.guild?.members.me.permissionsIn(message.channel as any) ?? new PermissionsBitField(8n);
+    this.applicationId = message.client.user.id;
+    this.channel = message.channel as any;
+    this.commandGuildId = message.guild.id;
+    this.commandType = ApplicationCommandType.ChatInput;
+    
+    this.client = message.client;
+    this.createdAt = message.createdAt;
+    this.createdTimestamp = message.createdTimestamp;
+    this.guild = message.guild;
+    this.guildId = message.guild?.id;
+    this.guildLocale = message.guild?.preferredLocale;
+    this.id = message.id;
+    this.locale = message.guild?.preferredLocale;
+    this.member = message.member;
+    this.memberPermissions = message.member?.permissions;
+
+    {
+      const argContent = message.content.slice(commandName.length).replace(/ +/, " ").trim();
+      const args = argContent.split(" ");
+
+      const options = chatInput.options;
+
+      for (let i = 0; i < args.length; i++) {
+        const arg = args[i];
+        const option = options[i];
+        if (!option) break;
+        this.parsedArgs.set(option.name, {
+          type: option.type,
+          value: arg
+        });
+      }
+    }
+  }
+
+  options: any = {
+    get: (name: string, type?: CommandOptionDataTypeResolvable) => {
+      const option = this.getOption(name);
+      if (!option) return null;
+      if (type && option.type !== type) return null;
+      return option.value;
+    }
+  }
+
+  private getOption(name: string): FakeMessageInteractionArgument {
+    return this.parsedArgs.get(name)?.value;
+  }
+
+  inGuild() {
+    return true;
+  }
+
+  async deferReply(options: any): Promise<any> {
+    if (options.ephemeral) throw new Error("Ephemeral replies are not supported in message commands.");
+    if (this.repliedMessage) throw new Error("Already deferred reply.");
+    this.repliedMessage = await this.message.reply("Loading...");
+    this.deferred = true;
+    return this.repliedMessage;
+  }
+
+  async deleteReply() {
+    if (!this.repliedMessage) throw new Error("No deferred reply.");
+    await this.repliedMessage.delete();
+    this.repliedMessage = undefined;
+  }
+
+  async followUp(content: string | MessagePayload) {
+    if (!this.repliedMessage) throw new Error("No deferred reply.");
+    if (!this.lastFollowUp) {
+      this.lastFollowUp = await this.repliedMessage.reply(content);
+    } else {
+      this.lastFollowUp = await this.lastFollowUp.reply(content);
+    }
+    return this.lastFollowUp;
+  }
+
+  async editReply(content: string | MessagePayload) {
+    if (!this.repliedMessage) throw new Error("No deferred reply.");
+    await this.repliedMessage.edit(content);
+    return this.repliedMessage;
+  }
+
+  async reply(content: any): Promise<any> {
+    if (this.repliedMessage) throw new Error("Already deferred reply.");
+    this.repliedMessage = await this.message.reply(content);
+    return this.repliedMessage;
+  }
+
+  async awaitModalSubmit() {
+    throw new Error("Method not implemented.");
+  };
+
+  async fetchReply() {
+    return this.repliedMessage?.id && await this.message.channel.messages.fetch(this.repliedMessage.id);
+  };
+}
+
+interface FakeMessageInteractionArgument {
+  type: ApplicationCommandOptionType,
+  value: any
+}
